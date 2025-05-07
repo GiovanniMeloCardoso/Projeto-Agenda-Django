acb11c1629ce31705646b18644c6d3ae70ffd9e2
Giovanni <giovannidemelo00@gmail.com>
2025-05-07 11:23:39 -0300

    Atualizei o README

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id','first_name', 'last_name', 'phone', 'email', 'created_date', 'description')
+    ordering = ('id',)
+    search_fields = ('first_name', 'last_name', 'phone', 'email', 'description')
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = ('first_name','last_name')
